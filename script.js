d47bbb5482ad1cc44d8a6216c180e6593e5d15fe
nabil <mahmudnabiliiuc@gmail.com>
2020-08-25 02:18:53 +0600

    first commit

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-let budget=0,income=0,expense=0
 let budget_ele = document.querySelector(".main_heading span")
 let income_ele = document.querySelector(".sub_heading1 span")
 let expense_ele = document.querySelector(".sub_heading2 span")
 
-budget_ele.innerHTML=budget
-income_ele.innerHTML=income
-expense_ele.innerHTML=expense
-
 let btn=document.querySelectorAll(".btn")
 let take_input=document.querySelectorAll(".take_input")
 let s_btn=document.querySelectorAll(".submit_btn")
+let disp_card=document.querySelectorAll(".sub_box")
 let value=document.querySelectorAll(".costing")
-
-
+let Discription=document.querySelectorAll(".description")
+let type=document.querySelectorAll(".type")
+let bal=document.querySelectorAll(".bal")
 
 
 btn[0].addEventListener("click",add_income)
@@ -31,10 +28,22 @@ function add_expense()
 s_btn[0].addEventListener("click",change_income)
 function change_income()
 {
-    
-}
+    disp_card[0].style.display="block"
+    take_input[0].style.display="none"
+    income_ele.innerHTML=value[0].value
+    budget_ele.innerHTML=value[0].value-value[1].value
+    bal[0].innerHTML=value[0].value
+    type[0].innerHTML= Discription[0].value
+    btn[0].style.display="block"
+}   
 s_btn[1].addEventListener("click",change_expense)
 function change_expense()
 {
-    
+    disp_card[1].style.display="block"
+    take_input[1].style.display="none"
+    expense_ele.innerHTML=value[1].value
+    budget_ele.innerHTML=value[0].value-value[1].value
+    bal[1].innerHTML=value[1].value
+    type[1].innerHTML= Discription[1].value
+    btn[1].style.display="block"
 }
